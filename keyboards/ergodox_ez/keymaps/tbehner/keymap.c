444535fc5e72b8de73f433c77e28d1c79a98114c
Timm Behner <timm.behner@gmail.com>
2025-01-16 17:02:51 +0100

    unify leader functionality; start on ergodox

--- keyboards/ergodox_ez/keymaps/tbehner/keymap.c
+++ keyboards/ergodox_ez/keymaps/tbehner/keymap.c
@@ -392,18 +392,4 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return state;
 };
 
-void leader_start_user(void) {
-    // Do something when the leader key is pressed
-}
-
-void leader_end_user(void) {
-    if (leader_sequence_one_key(KC_E)) {
-        // Leader, f => Types the below string
-        SEND_STRING(SS_TAP(X_ESC));
-    } else if (leader_sequence_one_key(KC_N)) {
-        SEND_STRING(SS_TAP(X_ENT));
-    } else if (leader_sequence_one_key(KC_T)) {
-        SEND_STRING(SS_TAP(X_TAB));
-    }
-
-}
+#include "../../../leader_functions/functions.c"
---
+++ leader_functions/functions.c
@@ -0,0 +1,16 @@
+
+void leader_start_user(void) {
+    // Do something when the leader key is pressed
+}
+
+void leader_end_user(void) {
+    if (leader_sequence_one_key(KC_E)) {
+        // Leader, f => Types the below string
+        SEND_STRING(SS_TAP(X_ESC));
+    } else if (leader_sequence_one_key(KC_N)) {
+        SEND_STRING(SS_TAP(X_ENT));
+    } else if (leader_sequence_one_key(KC_T)) {
+        SEND_STRING(SS_TAP(X_TAB));
+    }
+
+}
