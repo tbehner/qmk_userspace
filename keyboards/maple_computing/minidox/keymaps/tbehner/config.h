b9a9d6a25ae5a36b691cbf66e18ffc4261aee1f9
Timm Behner <timm.behner@gmail.com>
2025-02-18 08:23:27 +0100

    Updates minidox to the same state as the ergodox

--- keyboards/maple_computing/minidox/keymaps/tbehner/config.h
+++ keyboards/maple_computing/minidox/keymaps/tbehner/config.h
@@ -2,3 +2,6 @@
 
 
 #define COMBO_COUNT 3
+#define COMBO_TERM 30
+#define LEADER_TIMEOUT 250
+#define LEADER_PER_KEY_TIMING
--- keyboards/maple_computing/minidox/keymaps/tbehner/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/tbehner/keymap.c
@@ -76,8 +76,8 @@ enum custom_keycodes {
 #define MRGHT LCAG(KC_RGHT)
 
 const uint16_t PROGMEM esc_combo[] = { KC_L, KC_U, COMBO_END};
-const uint16_t PROGMEM tab_combo[] = { KC_N, KC_I, COMBO_END};
 const uint16_t PROGMEM ent_combo[] = { KC_U, KC_Y, COMBO_END};
+const uint16_t PROGMEM tab_combo[] = { KC_N, KC_I, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   COMBO(esc_combo, KC_ESC),
