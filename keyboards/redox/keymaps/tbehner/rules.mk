1f5be90aa169d48dcd709add910997de0a15fd5d
Timm Behner <timm.behner@gmail.com>
2025-05-16 16:00:16 +0200

    redox with lto

--- keyboards/redox/keymaps/tbehner/rules.mk
+++ keyboards/redox/keymaps/tbehner/rules.mk
@@ -13,7 +13,8 @@
 #CONSOLE_ENABLE = yes        # Console for debug
 #COMMAND_ENABLE = yes        # Commands for debug and configuration
 #NKRO_ENABLE = yes           # Enable N-Key Rollover
-#BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+LTO_ENABLE = yes
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 #RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 #AUDIO_ENABLE = no           # Audio output
 #CUSTOM_MATRIX = lite
